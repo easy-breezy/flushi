31ed13205f450f1a61015f7fe0779ce0946a3ee7
Dia6lo <Dia6lo@mail.ru>
2014-11-26 22:01:12 +1100

    Added bullet script

---
+++ Code/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCoTriggerEnter(Collider other) {
+		Debug.Log ("OK");
+		if (other.gameObject.tag == "Finish"){
+			GameObject explosionClone = (GameObject) Instantiate(explosion, other.transform.position, other.transform.rotation);
+			Destroy(explosionClone, 2);
+		}
+		Destroy (gameObject); // destroy the projectile anyway
+		Destroy(other.gameObject);
+	}
+}
