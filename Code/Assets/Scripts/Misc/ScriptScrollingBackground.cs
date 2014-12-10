27d1637155807d7c4897aebc2be472d9075925ff
Mehael <mehael@mail.ru>
2014-12-11 01:10:09 +1100

    Background Sprite

--- Code/Assets/Scripts/Misc/ScriptScrollingBackground.cs
+++ Code/Assets/Scripts/Misc/ScriptScrollingBackground.cs
@@ -1,23 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using UnityEngine;
+using System.Collections;
 public class ScriptScrollingBackground : MonoBehaviour {
-		
-    public float scrollSpeed;
-    private Vector2 savedOffset;
-		
-    void Start() {
-        savedOffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
-    }
-		
-    void Update() {
-        float y = Mathf.Repeat(Time.time * scrollSpeed, 1);
-        Vector2 offset = new Vector2(savedOffset.x, y);
-        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
-    }
-		
-    void OnDisable() {
-        renderer.sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
-    }
-
+	public float scrollSpeed;
+	public GameObject Flushi;
+	private Vector3 savedOffset;
+	
+	void Start () {
+		savedOffset = Flushi.transform.position;
+	}
+	
+	void Update () {
+		Vector2 offset = new Vector2 (Mathf.Repeat (Flushi.transform.position.x*scrollSpeed, 1), Mathf.Repeat (Flushi.transform.position.y*scrollSpeed, 1));
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+		transform.Translate( Flushi.transform.position - savedOffset);
+		savedOffset = Flushi.transform.position;
+	}
+	
+	void OnDisable () {
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", new Vector2());
+	}
 }
