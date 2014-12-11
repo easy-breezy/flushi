f58e29d79cceb56958a44ef8169d96db72c2274a
Mehael <mehael@mail.ru>
2014-12-11 13:13:48 +1100

    Added collider to bullets only after 0.3 sec after spawn to prevent self-collisions

--- Code/Assets/Scripts/Misc/ScriptScrollingBackground.cs
+++ Code/Assets/Scripts/Misc/ScriptScrollingBackground.cs
@@ -12,7 +12,7 @@ public class ScriptScrollingBackground : MonoBehaviour {
 		savedOffset = Flushi.transform.position;
 	}
 	
-	void Update () {
+	void FixedUpdate () {
 		Vector2 offset = new Vector2 (Mathf.Repeat (Flushi.transform.position.x*scrollSpeed, 1), Mathf.Repeat (Flushi.transform.position.y*scrollSpeed, 1));
 		renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
 		transform.Translate( Flushi.transform.position - savedOffset);
