6a92103ac8f6c3460d5b45613dc98780a0ff7d5d
Dia6lo <Dia6lo@mail.ru>
2015-04-10 23:57:58 +1100

    Move to Unity 5. Rename folders in /Scripts

---
+++ Code/Assets/Scripts/Controllers/ObjMovementController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ObjMovementController : MonoBehaviour
+{
+    public Vector2 Direction;
+    public float MoveSpeedLimit = 30f;
+    public float RotateSpeedLimit = 360f;
+
+    public void ApplyTorque(float value)
+    {
+            //if (value == 0) value = 10f;
+            gameObject.GetComponent<Rigidbody2D>().AddTorque(value);
+
+            if (Mathf.Abs(GetComponent<Rigidbody2D>().angularVelocity) > RotateSpeedLimit)
+                GetComponent<Rigidbody2D>().angularVelocity = Mathf.Sign(GetComponent<Rigidbody2D>().angularVelocity) * RotateSpeedLimit;
+    }
+
+    public void ApplyVelocity(float value)
+    {
+            if (value <= 0) return;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Direction.normalized * value);
+
+            if (GetComponent<Rigidbody2D>().velocity.magnitude > MoveSpeedLimit)
+                GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity.normalized * MoveSpeedLimit;
+    }
+
+    private void Start()
+    {
+    }
+
+    public virtual void Update()
+    {
+    }
+}
